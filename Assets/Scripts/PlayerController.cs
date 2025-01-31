8fda3fb9b458348a16dadd5f82449b824439f38f
Tristan Anderson <tristanlanderson@gmail.com>
2025-01-31 13:05:07 -0700

    Added in animal shelter and a few other objects. Testing exporting from blender objects, materials, animations and more.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,27 +3,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-  private Rigidbody rb;
-  private float movementX;
-  private float movementZ;
-
+  public CharacterController characterController;
+  public Transform playerCamera;
   public float speed = 0;
-  // Start is called once before the first execution of Update after the MonoBehaviour is created
-  void Start()
-  {
-    rb = GetComponent<Rigidbody>();
-  }
+  public float turnTime = 0.1f;
+  public GameObject spawnableItem;
+  private float turnVelocity;
 
-  private void FixedUpdate()
+  public void Update()
   {
-    Vector3 movement = new Vector3(movementX, 0.0f, movementZ);
-    rb.AddForce(movement * speed);
-  }
+    float horizontal = Input.GetAxisRaw("Horizontal");
+    float vertical = Input.GetAxisRaw("Vertical");
+    Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-  void OnMove(InputValue movementValue)
-  {
-    Vector2 movementVector = movementValue.Get<Vector2>();
-    movementX = movementVector.x;
-    movementZ = movementVector.y;
+    if (direction.magnitude >= 0.1f)
+    {
+      // Update rotation
+      float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+      float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnTime);
+      transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+      // Update movement
+      Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+      characterController.Move(moveDirection.normalized * speed * Time.deltaTime);
+    }
   }
 }
